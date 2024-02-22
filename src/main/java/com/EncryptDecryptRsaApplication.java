7e1a7668e97a3116116a3fbd68e7736684df9328
siddharth <siddkpany@gmail.com>
2024-02-22 12:12:51 +0530

    first commit

---
+++ src/main/java/com/EncryptDecryptRsaApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+
+import java.util.HashMap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptDecryptRsaApplication {
+
+    ;	public static void main(String[] args) {
+        SpringApplication.run(EncryptDecryptRsaApplication.class, args);
+    }
+
+}
