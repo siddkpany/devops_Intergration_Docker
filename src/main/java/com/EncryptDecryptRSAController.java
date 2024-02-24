eb6b14826395c0c3a6dff16711748feffa23212e
siddharth <siddkpany@gmail.com>
2024-02-24 12:27:17 +0530

    remove role

--- src/main/java/com/EncryptDecryptRSAController.java
+++ src/main/java/com/EncryptDecryptRSAController.java
@@ -1,13 +1,10 @@
 package com;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Role;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class EncryptDecryptRSAController {
@@ -17,20 +14,17 @@ public class EncryptDecryptRSAController {
     EncryptDecryptService encryptDecryptService;
 
     @GetMapping("/createKeys")
-    @RolesAllowed(value = "USER")
     public void createPrivatePublickey() {
         encryptDecryptService.createKeys();
     }
 
     @PostMapping("/encrpyt")
-    @RolesAllowed(value = "USER")
     public String encryptMessage(@RequestBody String plainString) {
         return encryptDecryptService.encryptMessage(plainString);
     }
 
 
     @PostMapping("/decrpyt")
-    @RolesAllowed(value = "USER")
     public String decryptMessage(@RequestBody String encryptString) {
         return encryptDecryptService.decryptMessage(encryptString);
     }
